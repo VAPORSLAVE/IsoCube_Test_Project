7b477922d96ce1b5cc384357827b4459de05f66c
VAPORSLAVE <40924546+VAPORSLAVE@users.noreply.github.com>
2024-01-19 16:53:27 -0800

    First Commit from Mac
    Test commit from Mac.

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Interactions;
+
+public class TestScript : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Hello World");
+    }
+}
